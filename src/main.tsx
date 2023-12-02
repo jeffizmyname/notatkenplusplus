07a00d74640a94010450c97b8a0b823f5c8707cf
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2023-12-02 23:37:00 +0100

    zmiany do dashboarda jescze

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'next-themes'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Dashboard from './Dasboard/'
+import Dashboard from './pages/Dasboard'
 import ErrorPage from './Error'
 import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
 import './index.css'
