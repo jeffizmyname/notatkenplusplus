ec9ebc2053ef5937688cd91ed089d87510397b93
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2023-12-04 23:01:00 +0100

    jakieś szkice

---
+++ src/pages/Home/Banner.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Banner() {
+    return (
+        <div className="md:flex md:items-center md:flex-col">
+            <div className="md:flex md:items-start md:flex-col">
+                <h1 className="text-7xl md:my-10 md:mt-40">Zorganizuj się</h1>
+                <p className="text-2xl text-foreground-300">Z łatwością zapisuj swoje pomysły i zadania aby o nich nie zapominec i mieć do nich dostęp w każdym miejscu</p>
+            </div>
+        </div>
+    )
+}
