26efff4bcd13e7db1397535e3cdfa8d1a9ccc9a2
jeffizmyname <stasiek1489@gmail.com>
2023-12-06 17:10:03 +0100

    lekkie css zmiany

--- src/pages/Home/Banner.tsx
+++ src/pages/Home/Banner.tsx
@@ -2,9 +2,9 @@
 
 export default function Banner() {
     return (
-        <div className="md:flex md:items-center md:flex-col">
-            <div className="md:flex md:items-start md:flex-col">
-                <h1 className="text-7xl md:my-10 md:mt-40">Zorganizuj się</h1>
+        <div className="md:flex md:items-center md:flex-col w-100%">
+            <div className="sm:mx-20 flex md:items-start sm:items-center flex-col w-100%">
+                <h1 className="text-7xl my-10 md:mt-40">Zorganizuj się</h1>
                 <p className="text-2xl text-foreground-300">Z łatwością zapisuj swoje pomysły i zadania aby o nich nie zapominec i mieć do nich dostęp w każdym miejscu</p>
             </div>
         </div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,6 +9,5 @@ export default function Home() {
             <Banner/>
             {/* <Footer/> */}
         </>
-
     )
 }
