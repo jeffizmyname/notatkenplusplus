46f74ed041e201ae0ed55641a127133c6a689119
jeffizmyname <stasiek1489@gmail.com>
2023-12-13 16:50:31 +0100

    to nieskanczone ale zmiana na menu

--- src/components/ThemeChagner.tsx
+++ src/components/ThemeChagner.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { useTheme } from 'next-themes'
-import { Switch } from '@nextui-org/react'
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Switch } from '@nextui-org/react'
 import { SunIcon } from '../assets/icons/SunIcon'
 import { MoonIcon } from '../assets/icons/MoonIcon'
 
 export const ThemeChanger = () => {
     const { theme, setTheme } = useTheme()
     const [isSelected, setIsSelected] = React.useState(theme == 'light' ? true : false);
+
+    const [selectedKeys, setSelectedKeys] = React.useState(new Set(["text"]));
+
+    const selectedValue = React.useMemo(
+        () => Array.from(selectedKeys).join(", ").replaceAll("_", " "),
+        [selectedKeys]
+    );
+
+    function chagne(keys) {
+        setIsSelected(theme == 'light' ? true : false)
+        setSelectedKeys(keys)
+    }
 
     return (
         <div className=''>
@@ -24,6 +36,27 @@ export const ThemeChanger = () => {
                     )
                 }>
             </Switch>
+            <Dropdown>
+                <DropdownTrigger>
+                    <Button 
+                        variant="flat"
+                        isIconOnly>
+                    {selectedValue == "dark" ? <MoonIcon/> : <SunIcon/>}
+                    </Button>
+                </DropdownTrigger>
+                <DropdownMenu
+                selectionMode="single"
+                disallowEmptySelection
+                selectedKeys={selectedKeys}
+                onSelectionChange={setIsSelected}>
+                    <DropdownItem key="dark">
+                        Dark
+                    </DropdownItem>
+                    <DropdownItem key="light">
+                        Light
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
         </div>
     )
 }
