511ea48c6eebf4a62e762801fcb6881b0cc3b4b8
jeffizmyname <stasiek1489@gmail.com>
2023-12-13 16:52:39 +0100

    konto w dashboard

--- src/pages/Dasboard/NavBar.tsx
+++ src/pages/Dasboard/NavBar.tsx
@@ -1,8 +1,11 @@
 import {Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@nextui-org/react";
+import Account from "../Home/Account";
 
 export default function NavBar() {
     return (
         <Navbar
-        position="static"></Navbar>
+        position="static">
+            <Account/>
+        </Navbar>
     )
 }
