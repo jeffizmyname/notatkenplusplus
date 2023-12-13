214ebd200eede01b7edcd9b2b1b2dda0367039c7
jeffizmyname <stasiek1489@gmail.com>
2023-12-13 16:53:38 +0100

    linki do rzeczy co beda działac kiedys i wyswietlanie tego co ma myswietlać

--- src/pages/Dasboard/Content.tsx
+++ src/pages/Dasboard/Content.tsx
@@ -1,32 +1,53 @@
 import { Card } from "@nextui-org/card";
+import { Outlet } from "react-router-dom";
+import {useLayoutEffect, useRef, useState} from 'react';
 
 export default function Content() {
+
+    const ref = useRef<any>(null);
+
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+
+    useLayoutEffect(() => {
+        if(ref.current !== null) {
+            setWidth(ref.current.offsetWidth);
+            setHeight(ref.current.offsetHeight);
+        }
+    }, []);
+
+    sessionStorage.setItem("paintWH", `{"width": ${width}, "height": ${height}}`)
+
+    //console.log(width + " " + height)
+
     return(
-        <div className="w-[85%] bg-red-500">
-            <p>Today</p>
-            <div>
-                <Card>
-                    <p>HUJ</p>
-                </Card>
-                <Card>
-                <p>HUJ</p>
-                </Card>
-                <Card>
-                <p>HUJ</p>
-                </Card>
-            </div>
-            <p>Yesterday</p>
-            <div>
-                <Card>
-                <p>HUJ</p>
-                </Card>
-                <Card>
-                <p>HUJ</p>
-                </Card>
-                <Card>
-                <p>HUJ</p>
-                </Card>
-            </div>
+        <div className="w-[85%] bg-background/50" ref={ref}>
+            <Outlet />
         </div>
     )
 }
+
+{/* <p>Today</p>
+<div>
+    <Card>
+        <p>EEEEEE</p>
+    </Card>
+    <Card>
+    <p>EEEEEE</p>
+    </Card>
+    <Card>
+    <p>EEEEEE</p>
+    </Card>
+</div>
+<p>Yesterday</p>
+<div>
+    <Card>
+    <p>EEEEEE</p>
+    </Card>
+    <Card>
+    <p>EEEEEE</p>
+    </Card>
+    <Card>
+    <p>EEEEEE</p>
+    </Card>
+</div> */}
--- src/pages/Dasboard/LeftBar.tsx
+++ src/pages/Dasboard/LeftBar.tsx
@@ -1,7 +1,19 @@
+import { Link } from "@nextui-org/react";
 
 export default function LeftBar() {
     return (
         <div className="h-full w-[15%] bg-background">
+            <div className="flex flex-col my-2 ml-5">
+                <Link href="/Dashboard/MyThings">Moje Rzeczy</Link>
+                <Link href="/Dashboard/Calendar">Kalendarz</Link>
+            </div>
+            <div className="flex flex-col my-2 ml-5">
+                <Link href="/Dashboard/Blank">Nowa Notatka</Link>
+                <Link href="/Dashboard/Paint">Nowe Płutno</Link>
+                <Link href="/Dashboard/ToDo">Nowa Lista TODO</Link>
+                <Link href="/Dashboard/Chart">Nowy Wykres</Link>
+            </div>
+
         </div>
     )
 }
