2d8bd065ec3f20ff543cdab19968f79b2671517a
jeffizmyname <stasiek1489@gmail.com>
2023-12-13 16:52:26 +0100

    pokazuje przycisk dashboard tylko kiedy na stronie głowniej

--- src/pages/Home/Account.tsx
+++ src/pages/Home/Account.tsx
@@ -1,9 +1,10 @@
 import { NavbarContent, NavbarItem, Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Link, Button } from "@nextui-org/react";
 import { ThemeChanger } from "../../components/ThemeChagner";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function Account() {
     const navigate = useNavigate();
+    const location = useLocation();
     const data = sessionStorage.getItem("userData")
     let email = "";
     if (data !== null) {
@@ -13,9 +14,12 @@ export default function Account() {
 
     return (
         <NavbarContent as="div" justify="end">
-            <NavbarItem>
-            <Button as={Link} color="primary" variant="flat" href="/dashboard">Dashboard</Button>
-            </NavbarItem>
+            {location.pathname === "/" ? 
+                        <NavbarItem>
+                        <Button as={Link} color="primary" variant="flat" href="/dashboard">Dashboard</Button>
+                        </NavbarItem>
+                        : <></>}
+
             <Dropdown placement="bottom-end">
                 <DropdownTrigger>
                     <Avatar
@@ -33,11 +37,7 @@ export default function Account() {
                         <p className="font-semibold">Signed in as</p>
                         <p className="font-semibold">{email}</p>
                     </DropdownItem>
-                    <DropdownItem key="settings">My Settings</DropdownItem>
-                    <DropdownItem key="team_settings">Team Settings</DropdownItem>
-                    <DropdownItem key="analytics">Analytics</DropdownItem>
-                    <DropdownItem key="system">System</DropdownItem>
-                    <DropdownItem key="configurations">Configurations</DropdownItem>
+                    <DropdownItem key="settings">Settings</DropdownItem>
                     <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
                     <DropdownItem key="logout" color="danger" onClick={() => {sessionStorage.setItem("loggedIn", "false"); navigate("/")}}>
                         Log Out
