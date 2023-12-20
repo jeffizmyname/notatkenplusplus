e20455b2de03a58e0a9f3d8916a5f87c82c15d1b
jeffizmyname <stasiek1489@gmail.com>
2023-12-20 17:05:44 +0100

    to bedzie todo

--- src/pages/Dasboard/Options/ToDo.tsx
+++ src/pages/Dasboard/Options/ToDo.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 export default function ToDo() {
+
+    const [todos, setTodos] = useState();
+    const [input, setInput] = useState('');
+
+    const addTodo = (e) => {
+        e.preventDefault();
+        setTodos([...todos, input]);
+        setInput('')
+    };
+
     return (
-        <p>ToDo</p>
+        <div>
+            <h1>ToDo Name</h1>
+            
+        </div>
     )
 }
