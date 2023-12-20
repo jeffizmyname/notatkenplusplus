89379ab3f38fbd44a016d30331715d53fe8f5e52
jeffizmyname <stasiek1489@gmail.com>
2023-12-20 17:05:36 +0100

    zmiania routingu

--- src/pages/Dasboard/LeftBar.tsx
+++ src/pages/Dasboard/LeftBar.tsx
@@ -8,10 +8,10 @@ export default function LeftBar() {
                 <Link href="/Dashboard/Calendar">Kalendarz</Link>
             </div>
             <div className="flex flex-col my-2 ml-5">
-                <Link href="/Dashboard/Blank">Nowa Notatka</Link>
-                <Link href="/Dashboard/Paint">Nowe Płutno</Link>
-                <Link href="/Dashboard/ToDo">Nowa Lista TODO</Link>
-                <Link href="/Dashboard/Chart">Nowy Wykres</Link>
+                <Link href="/Dashboard/BlankFiles">Nowa Notatka</Link>
+                <Link href="/Dashboard/PaintFiles">Nowe Płutno</Link>
+                <Link href="/Dashboard/ToDoFiles">Nowa Lista TODO</Link>
+                <Link href="/Dashboard/ChartFiles">Nowy Wykres</Link>
             </div>
 
         </div>
