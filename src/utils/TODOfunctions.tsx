051bb18cae04891cf00e390ba9a9018ae0b1b342
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2023-12-27 04:49:04 +0100

    fukcje do todo

---
+++ src/utils/TODOfunctions.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export function createTODO(userId: number, todoName: string, todoAuthor: string, todoDesc: string) {
+    const todoData = {
+        user_id: userId,
+        Name: todoName,
+        Author: todoAuthor,
+        Description: todoDesc,
+    }
+    axios.post("http://localhost:3001/todos", todoData)
+    .then(() => {
+        alert("TODO Created")
+    })
+    .catch(error => {console.log("error: " + error)})
+}
+
+export async function getUserTODOS(userId: number) {
+    try {
+        const response = await axios.get(`http://localhost:3001/todos/${userId}`);
+        return response.data.userData;
+    } catch (error) {
+        console.error('Error fetching user todos:', error);
+        throw error;
+    }
+}
