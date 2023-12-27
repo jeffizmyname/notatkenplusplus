f2e14cededa6cfe0bb197c7c6f68dac9a993d59e
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2023-12-27 04:48:42 +0100

    todo zrbione prawie tylko checkbox napraw i style

--- src/pages/Dasboard/FilesSites/ToDoFiles/ToDoFiles.tsx
+++ src/pages/Dasboard/FilesSites/ToDoFiles/ToDoFiles.tsx
@@ -1,36 +1,50 @@
+import { useEffect, useState } from "react";
 import CardElement from "../../../../components/CardElement";
+import { getUserTODOS } from "../../../../utils/TODOfunctions";
+import { getId } from "../../../../utils/userData";
 
-
-
+interface TodoItem {
+  Name: string;
+  Author: string;
+  Description: string;
+}
 
 export default function ToDoFiles() {
-    return (
+  const [userTODOs, setUserTODOs] = useState<TodoItem[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const userData = await getUserTODOS(getId()!);
+        setUserTODOs(userData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(userTODOs);
+
+
+  return (
+    <div>
       <div>
-        <div>
-          <p>Dzisiaj</p>
-          <div className="flex flex-row gap-5">
-            <CardElement type="new"/>
-            <CardElement type="TODO"/>
-            <CardElement type="TODO"/>
-            <CardElement type="TODO"/>
-          </div>
-        </div>
-        <div >
-          <p>Wczoraj</p>
-          <div className="flex flex-row gap-5">
-            <CardElement type="TODO"/>
-            <CardElement type="TODO"/>
-            <CardElement type="TODO"/>
-          </div>
-        </div>
-        <div>
-          <p>1.1.2023</p>
-          <div className="flex flex-row gap-5">
-            <CardElement type="TODO"/>
-            <CardElement type="TODO"/>
-            <CardElement type="TODO"/>
-          </div>
+        <p>Dzisiaj</p>
+        <div className="flex flex-row gap-5">
+        <CardElement type="new" ListName="" Author="" Desc="" />
+          {userTODOs.map((todo, index) => (
+            <CardElement
+              key={index}
+              type="TODO"
+              ListName={todo.Name} 
+              Author={todo.Author}
+              Desc={todo.Description} 
+            />
+          ))}
         </div>
       </div>
-    )  
-  }
+    </div>
+  )
+}
--- src/pages/Dasboard/Options/ToDo.tsx
+++ src/pages/Dasboard/Options/ToDo.tsx
@@ -1,20 +1,118 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import { getUserTODOS } from '../../../utils/TODOfunctions';
+import { getId } from '../../../utils/userData';
+import { useParams } from 'react-router-dom';
+import { Button, Checkbox, CheckboxGroup, Input } from '@nextui-org/react';
+import axios from 'axios';
 
-export default function ToDo() {
+interface Todo {
+    id: number;
+    user_id: number;
+    Name: string;
+    Author: string;
+    Description: string;
+    Data: Record<string, unknown> | null;
+}
 
-    const [todos, setTodos] = useState();
-    const [input, setInput] = useState('');
+function ToDo() {
+    const TODOid = useParams()
+    const [todoData, setTodoData] = useState<Todo | null>(null);
+    const [entries, setEntries] = useState<{ isDone: boolean; task: string }[]>([]);
 
-    const addTodo = (e) => {
-        e.preventDefault();
-        setTodos([...todos, input]);
-        setInput('')
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const userData = await getUserTODOS(getId()!);
+                const resultObject = userData.find((item: Todo) => item.id === Number(TODOid.todoID));
+                setTodoData(resultObject);
+                if (resultObject && resultObject.Data) {
+                    const unescapedString = resultObject.Data.replace(/\\"/g, '"');
+                    const jsonArray = JSON.parse(unescapedString);
+                    setEntries(jsonArray);
+                }
+                console.log(entries)
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    function addField() {
+        setEntries((prevEntries) => [...prevEntries, { isDone: false, task: '' }]);
+        console.log(entries)
+    }
+
+    const handleTaskChange = (index: number, value: string) => {
+        setEntries((prevEntries) =>
+            prevEntries.map((entry, i) => (i === index ? { ...entry, task: value } : entry))
+        );
     };
+
+    const handleCheckboxChange = (index: number, isChecked: boolean) => {
+        setEntries((prevEntries) =>
+            prevEntries.map((entry, i) => (i === index ? { ...entry, isDone: isChecked } : entry))
+        );
+    };
+
+    const handleDelete = (index: number) => {
+        setEntries((prevEntries) => prevEntries.filter((entry, i) => i !== index));
+    };
+
+    const handleSave = async () => {
+        try {
+            const jsonData = JSON.stringify(entries);
+            await axios.post('http://localhost:3001/todos/update', {
+                id: todoData?.id,
+                user_id: getId()!,
+                Name: todoData?.Name,
+                Author: todoData?.Author,
+                Description: todoData?.Description,
+                Data: jsonData,
+            });
+
+            console.log('Todo updated successfully');
+        } catch (error) {
+            console.error('Error updating todo:', error);
+        }
+    };
+
+
 
     return (
         <div>
-            <h1>ToDo Name</h1>
-            
+            <div>
+                {todoData && (
+                    <>
+                        <p>Nazwa: {todoData.Name}</p>
+                        <p>Autor: {todoData.Author}</p>
+                        <p>Opis: {todoData.Description}</p>
+                    </>
+                )}
+            </div>
+            <div>
+                <p>wpisy</p>
+                <CheckboxGroup>
+                    {entries.map((entry, index) => (
+                        <div key={index} className='w-[250px] flex row'>
+                            <Checkbox isSelected={entry.isDone}
+                                onValueChange={(isChecked) => { handleCheckboxChange(index, isChecked) }}
+                                value={String(index)} />
+                            <Input
+                                defaultValue={entry.task}
+                                onChange={(e) => handleTaskChange(index, e.target.value)} />
+                            <Button onClick={() => handleDelete(index)}>Delete</Button>
+                        </div>
+                    ))}
+                </CheckboxGroup>
+            </div>
+            <div>
+                <Button onClick={addField}>Nowy wpis</Button>
+                <Button onClick={handleSave}>Zapisz</Button>
+            </div>
         </div>
-    )
+    );
 }
+
+export default ToDo;
