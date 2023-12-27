6662bb22d4a9ac83b2f8ff3870a504238d5d50e3
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2023-12-27 04:48:14 +0100

    karta wyswietla to co ma wyswietlac

--- src/components/CardElement.tsx
+++ src/components/CardElement.tsx
@@ -1,76 +1,118 @@
-import { Card, CardFooter, Image, Modal, ModalContent, ModalHeader, ModalBody,  useDisclosure, ModalFooter, Button } from "@nextui-org/react";
+import { Card, CardFooter, Image, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input, Textarea } from "@nextui-org/react";
+import { createTODO } from "../utils/TODOfunctions";
+import { useState } from "react";
+import { getId } from "../utils/userData";
 
 type Props = {
     type: string
+    ListName: string,
+    Author: string,
+    Desc: string
+}
+
+interface todoForm {
+    ListName?: string,
+    Author?: string,
+    Desc?: string
 }
 
 
 export default function CardElement(props: Props) {
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
-    const CardType = props.type
+    const [fields, setFields] = useState<todoForm>({});
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const CardType = props.type;
+
+    const handleChange = (field: string, value: string) => {
+        setFields({
+            ...fields,
+            [field]: value
+        })
+    }
+
     return (
         <>
-            <Card 
-            isFooterBlurred
-            isPressable 
-            onPress={onOpen}
-            className="w-[200px] h-[200px]">
+            <Card
+                isFooterBlurred
+                isPressable
+                onPress={onOpen}
+                className="w-[200px] h-[200px]">
 
-                {
-                CardType !== "new" ? 
-                <>
-                <Image
-                alt="preview test"
-                src="https://picsum.photos/200"
-                width={200}>
-                </Image>
-                
-                <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-                    Nazwa Czy cosa takiego
-                </CardFooter> 
-                </>: 
-                <Button className="w-[200px] h-[200px]">New</Button>}
-
-            </Card>
-            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-            <ModalContent>
-                {(onClose) => (
+                {CardType !== "new" ? (
                     <>
-                    <ModalHeader className="flex flex-col gap-1">X name</ModalHeader>
-                    <ModalBody>
-                        <p> 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam pulvinar risus non risus hendrerit venenatis.
-                        Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                        </p>
-                        <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam pulvinar risus non risus hendrerit venenatis.
-                        Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                        </p>
-                        <p>
-                        Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
-                        dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. 
-                        Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. 
-                        Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur 
-                        proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
-                        </p>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="danger" variant="light" onPress={onClose}>
-                        Close
-                        </Button>
-                        <Button color="primary" variant="light" onPress={onClose}>
-                        Share
-                        </Button>
-                        <Button color="primary" onPress={onClose}>
-                        Open
-                        </Button>
-                    </ModalFooter>
+                        <Image
+                            alt="preview test"
+                            src="https://picsum.photos/200"
+                            width={200}
+                        />
+                        <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                            {props.ListName}
+                        </CardFooter>
                     </>
+                ) : (
+                    <Button className="w-[20px] h-[20px]">New</Button>
                 )}
+            </Card>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalContent>
+                    {CardType !== "new" ? (
+                        <ModalDefaultContent onClose={onClose} name={props.ListName} author={props.Author} desc={props.Desc}/>
+                    ) : (
+                        <ModalNewContent onClose={onClose} handleChange={handleChange} fields={fields} />
+                    )}
                 </ModalContent>
             </Modal>
         </>
-    )
+    );
 }
+
+type ModalDefaultContentProps = {
+    onClose: () => void;
+    name: string;  // Updated from ListName to name
+    author: string;  // Updated from Author to author
+    desc: string;  // Updated from Desc to desc
+};
+
+const ModalDefaultContent: React.FC<ModalDefaultContentProps> = ({ onClose, name, author, desc }) => (
+    <>
+        <ModalHeader className="flex flex-col gap-1">{name}</ModalHeader>
+        <ModalBody>
+            <p>{author}</p>
+            <p>{desc}</p>
+        </ModalBody>
+        <ModalFooter>
+            <Button color="danger" variant="light" onPress={onClose}>
+                Close
+            </Button>
+            <Button color="primary" variant="light" onPress={onClose}>
+                Share
+            </Button>
+            <Button color="primary" onPress={onClose}>
+                Open
+            </Button>
+        </ModalFooter>
+    </>
+);
+
+
+const ModalNewContent: React.FC<{ onClose: () => void; handleChange: (field: string, value: string) => void; fields: todoForm }> = ({ onClose, handleChange, fields }) => (
+    <>
+        <ModalHeader className="flex flex-col gap-1">Nowa Lista</ModalHeader>
+        <ModalBody>
+            <Input label="Nazwa Listy" onChange={e => handleChange('ListName', e.target.value)} value={fields.ListName || ''}></Input>
+            <Input label="Autor" onChange={e => handleChange('Author', e.target.value)} value={fields.Author || ''}></Input>
+            <Textarea
+                label="Opis listy"
+                className="max-w-m"
+                onChange={e => handleChange('Desc', e.target.value)} value={fields.Desc || ''}
+            />
+        </ModalBody>
+        <ModalFooter>
+            <Button color="danger" variant="light" onPress={onClose}>
+                Close
+            </Button>
+            <Button color="primary" onPress={() => createTODO(getId()!, fields.ListName || '', fields.Author || '', fields.Desc || '')}>
+                Create
+            </Button>
+        </ModalFooter>
+    </>
+);
