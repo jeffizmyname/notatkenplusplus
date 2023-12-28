a6139263860c58b77f126f973d73b846adead847
jeffizmyname <stasiek1489@gmail.com>
2023-12-28 18:21:48 +0100

    wykresy tylok biblioteki nie pobrałem

--- src/pages/Dasboard/Options/Chart.tsx
+++ src/pages/Dasboard/Options/Chart.tsx
@@ -1,5 +1,15 @@
-export default function Chart() {
-    return (
-        <p>Chart</p>
-    )
-}
+// import { LineChart, Line } from 'recharts';
+// const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }];
+
+// const renderLineChart = (
+//     <LineChart width={400} height={400} data={data}>
+//         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+//     </LineChart>
+// );
+
+// export default function Chart() {
+//     return (
+//         {renderLineChart}
+//     )
+// }
+//! zainstaluj importa
