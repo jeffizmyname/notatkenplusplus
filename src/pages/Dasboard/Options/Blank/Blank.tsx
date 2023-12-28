15371826e4b758f19183a198393c835d7ae08d9d
jeffizmyname <stasiek1489@gmail.com>
2023-12-28 18:22:46 +0100

    dodanie opcji zapisywanie i dcytywania tekstu

--- src/pages/Dasboard/Options/Blank/Blank.tsx
+++ src/pages/Dasboard/Options/Blank/Blank.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 import ReactQuill from "react-quill";
 import { Delta as TypeDelta, Sources } from 'quill';
 import Delta from "quill-delta";
 import "react-quill/dist/quill.snow.css";
 import EditorToolBar, { formats, modules } from "./EditorToolBar";
 import { Button } from "@nextui-org/react";
+import { getElements, handleSave } from "../../../../utils/saveLoad";
+import { getId } from "../../../../utils/userData";
 
-const delta = (new Delta([
+let delta = (new Delta([
     {
         "attributes": {
             "color": "#e60000"
@@ -31,19 +34,50 @@ const delta = (new Delta([
     },
 ]) as unknown) as TypeDelta;
 
+interface Note {
+    id: number;
+    user_id: number;
+    Name: string;
+    Author: string;
+    Description: string;
+    Data: Record<string, unknown> | null;
+}
+
+
 export default function MyComponent() {
+    const Blankid = useParams()
+    const [NoteData, setNoteData] = useState<Note | null>(null);
     const [value, setValue] = useState(delta);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const userData = await getElements("blank", getId()!)
+                const resultObject = userData.find((item: Note) => item.id === Number(Blankid.blankID));
+                setNoteData(resultObject);
+                console.log(resultObject)
+                if (resultObject && resultObject.Data) {
+                    const unescapedString = resultObject.Data.replace(/\\"/g, '"');
+                    const jsonArray = JSON.parse(unescapedString);
+                    delta = (new Delta(jsonArray) as unknown) as TypeDelta;
+                    setValue(delta);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
     const onEditorChange = (value: string, delta: TypeDelta, source: Sources, editor: ReactQuill.UnprivilegedEditor) => {
         setValue(editor.getContents());
     };
 
-    const handleSave = () => {
-        // Get the current contents of the editor
+    const HandleSave = () => {
         const editorContents = value;
-        // Do something with the contents, e.g., save it to a database or perform further processing
         console.log("Saved content:", editorContents);
-        // Add your save logic here
-        //!zapisz to jakos do bazy danych a potem da sie oczytac ta deltą czy coś XD
+        handleSave("blank", editorContents, NoteData)
     };
 
     return (
@@ -56,7 +90,7 @@ export default function MyComponent() {
         modules={modules}
         formats={formats}
         />
-        <Button onClick={handleSave}>Save</Button>
+        <Button onClick={HandleSave}>Save</Button>
     </div>);
 }
 
