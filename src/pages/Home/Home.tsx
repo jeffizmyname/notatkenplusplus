ba7533cc9f6fd3c8c069953867c8d3e093841861
jeffizmyname <stasiek1489@gmail.com>
2023-12-30 22:51:50 -0400

    dodenie sekcji z kodem

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,15 @@
 import NavBar from "./NavBar"
 import Footer from "./Footer"
 import Banner from "./Banner"
+import Code from "./Code"
 
 export default function Home() {
     return (
-        <>
+        <div className="h-fit">
             <NavBar/>
             <Banner/>
+            <Code/>
             {/* <Footer/> */}
-        </>
+        </div>
     )
 }
