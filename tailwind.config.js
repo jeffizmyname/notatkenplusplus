4bda8602ffa8ccd4c52ecd3a16cd703365ace235
jeffizmyname <stasiek1489@gmail.com>
2023-12-30 22:50:15 -0400

    maly ekran do tilwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const { nextui } = require("@nextui-org/react");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
@@ -7,6 +8,10 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    screens: {
+      'xs': '300px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   darkMode: "class",
