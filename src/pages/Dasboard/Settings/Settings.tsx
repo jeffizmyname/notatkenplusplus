932bbf4fb0ed1cbd8e8b20421a24c971487a3917
jeffizmyname <stasiek1489@gmail.com>
2024-01-03 01:00:18 -0400

    zaczołem robić strone z ustawieniami

--- src/pages/Dasboard/Settings/Settings.tsx
+++ src/pages/Dasboard/Settings/Settings.tsx
@@ -1,6 +1,63 @@
+import { Button, Card, CardBody, CardFooter, CardHeader, Checkbox, Input, Link, Modal, ModalBody, ModalContent, ModalHeader, useDisclosure } from "@nextui-org/react";
 
 export default function Settings() {
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    //tak zeby 2 modale działały
+
+
     return (
-        <p>Settings</p>
+        <div>
+            <p>zmiana avatara / wysylaj emaile z nowosciami / zmiana hasla / zmiani emaila /  usun konto</p>
+            <p>Ustawienia</p>
+            <Card className="w-[500px]">
+                <CardHeader>
+                    <p>Edytuj konto</p>
+                </CardHeader>
+                <CardBody>
+                    <Input
+                    label="email"/>
+                    <Input
+                    type="file"
+                    label="avatar"/>
+                </CardBody>
+                <CardFooter className="flex justify-between">
+                    <Link onClick={onOpen} underline="always">Chce zmienić hasło</Link>
+                    <Button color="primary" variant="flat">Zmień</Button>
+                    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                        <ModalContent>
+                            <ModalBody>
+
+                            </ModalBody>
+                        </ModalContent>
+                    </Modal>
+                </CardFooter>
+            </Card>
+            <Card className="w-[500px]">
+                <CardBody className="flex flex-row justify-between">
+                    <Checkbox>Chce otrymywać newslettera z nowościami</Checkbox>
+                    <Button color="primary" variant="flat">Zapisz</Button>
+                </CardBody>
+            </Card>
+            <Card className="w-[500px]">
+                <CardHeader>
+                    <p>Usuń konto</p>
+                </CardHeader>
+                <CardBody >
+                    <Checkbox color="danger">Jestem świadomy/a że usunięcie konta wiąże sie z całkowitą bezpowrotną utratą danych</Checkbox>
+                </CardBody>
+                <CardFooter>
+                    <Button color="danger" variant="flat" onClick={onOpen}>Usuń</Button>
+                    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                        <ModalContent>
+                            <ModalHeader>Czy jeteś pewien/pewna</ModalHeader>
+                            <ModalBody className="flex flex-row">
+                                <Button color="danger" variant="flat" className="w-1/2">Tak</Button>
+                                <Button variant="flat" className="w-1/2" onClick={onOpenChange}>Nie</Button>
+                            </ModalBody>
+                        </ModalContent>
+                    </Modal>
+                </CardFooter>
+            </Card>
+        </div>
     )
 }
