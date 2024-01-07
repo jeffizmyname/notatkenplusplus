1a836186cef407eb768a552b6d791e16ade3c55d
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-01-07 22:15:36 +0100

    css zeby bylo wiekosci strony

--- src/pages/Dasboard/Content.tsx
+++ src/pages/Dasboard/Content.tsx
@@ -20,7 +20,7 @@ export default function Content() {
     //console.log(width + " " + height)
 
     return(
-        <div className="w-full lg:w-[85%] bg-background/50 h-[2000px]" ref={ref}>
+        <div className="w-full lg:w-[85%] bg-background/50 h-full" ref={ref}>
             <Outlet />
         </div>
     )
