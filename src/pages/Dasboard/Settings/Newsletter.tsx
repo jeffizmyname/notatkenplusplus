0bd84d33f7de0a535a9750cd8692eb64235ab35e
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-01-07 05:25:00 +0100

    zeby zapisywanie newslwettera działło

--- src/pages/Dasboard/Settings/Newsletter.tsx
+++ src/pages/Dasboard/Settings/Newsletter.tsx
@@ -1,18 +1,27 @@
 import { Card, CardBody, Checkbox, Button } from "@nextui-org/react";
 import axios from "axios";
 import { getId } from "../../../utils/userData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Newsletter() {
     const [isSelected, setIsSelected] = useState(false);
+    const id = getId();
+
     
     const handleNewsletter = () => {
-        const id = getId();
-        console.log(isSelected + " " + id)
-        axios.post("http://localhost:3001/settings/newsletter", {isSelected, id})
+        axios.post("http://localhost:3001/settings/newsletterChange", {isSelected, id})
         .catch(err => console.log(err))
     }
 
+    const reciveNewsletter = () => {
+        axios.post("http://localhost:3001/settings/newsletter", {id})
+        .then(response => setIsSelected(response.data.res[0].newsletter))
+        .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        reciveNewsletter()
+    }, [])
 
     return (
         <Card className="h-[65px]">
