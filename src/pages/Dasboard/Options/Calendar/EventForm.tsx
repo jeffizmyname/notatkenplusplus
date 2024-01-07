ea8e8a993853e32f6bca8066adf7d9b9c7bd497e
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-01-07 05:24:26 +0100

    formularz do eventow i je mozna dodawc oczywiscie

--- src/pages/Dasboard/Options/Calendar/EventForm.tsx
+++ src/pages/Dasboard/Options/Calendar/EventForm.tsx
@@ -1,22 +1,68 @@
 import { Input, Button, ModalBody, ModalFooter, ModalHeader, Textarea} from '@nextui-org/react';
+import axios from 'axios';
+import { format } from 'date-fns';
+import { useState } from 'react';
+import { getId } from '../../../../utils/userData';
+
+interface Fields {
+    type?: string,
+    title?: string,
+    start?: string,
+    end?: string,
+    desc?: string,
+    date?: string
+}
+
+interface Props {
+    today: Date
+}
+
+export default function EventForm(props: Props) {
+    const [fields, setFields] = useState<Fields>({"type": "event", "date": format(props.today, "yyyy-MM-dd")});
+
+    const handleChange = (field: string, value: string) => {
+        setFields({
+            ...fields,
+            [field]: value
+        })
+    }
 
 
-export default function EventForm() {
+    const handleEvent = () => {
+        const id = getId();
+        const string = JSON.stringify(fields)
+        handleChange("type", "event")
+        handleChange("date", format(props.today, "yyyy-MM-dd"))
+        console.log(fields)
+        axios.post("http://localhost:3001/calendar/addTask", {id, string})
+        .catch(err => console.log(err))
+    }
 
     return (
             <>
                 <ModalHeader>Nowe wydarzenie</ModalHeader>
                 <ModalBody>
                     <Input
-                    label="nazwa wydarzenia"/>
+                    type='text'
+                    label="nazwa wydarzenia"
+                    onChange={(e) => handleChange('title', e.target.value)}
+                    value={fields['title']}/>
                     <div className='flex flex-row gap-3'>
                         <Input
-                        type='time'/>
+                        type='time'
+                        startContent="od"
+                        onChange={e => handleChange('start', e.target.value)}
+                        value={fields['start']}/>
                         <Input
-                        type='time'/>
+                        startContent="do"
+                        type='time'
+                        onChange={e => handleChange('end', e.target.value)}
+                        value={fields['end']}/>
                     </div>
                     <Textarea
-                    label="opis"/>
+                    label="opis"
+                    onChange={e => handleChange('desc', e.target.value)}
+                    value={fields['desc']}/>
                 </ModalBody>
                 <ModalFooter>
                     <Button
@@ -26,7 +72,8 @@ export default function EventForm() {
                     <Button
                     className='w-1/2'
                     variant='flat'
-                    color='primary'>Dodaj</Button>
+                    color='primary'
+                    onClick={handleEvent}>Dodaj</Button>
                 </ModalFooter>
             </>
     )
