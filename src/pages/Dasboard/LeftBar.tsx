682d2bd9764f3ce262356b805ad01dfb6b38e451
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-01-09 00:27:24 +0100

    nastepne cesese

--- src/pages/Dasboard/LeftBar.tsx
+++ src/pages/Dasboard/LeftBar.tsx
@@ -2,7 +2,7 @@ import { Link } from "@nextui-org/react";
 
 export default function LeftBar() {
     return (
-        <div className="h-full w-[15%] bg-background lg:block sm:hidden xs:hidden">
+        <div className="w-[15%] bg-background lg:block sm:hidden xs:hidden">
                 <div className="flex flex-col my-2 ml-5">
                     <Link href="/Dashboard/MyThings">Moje Rzeczy</Link>
                     <Link href="/Dashboard/Calendar">Kalendarz</Link>
