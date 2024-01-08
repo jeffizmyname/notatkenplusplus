e135cd6cf2142320e86de530dec33fd4aae49184
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-01-09 00:26:02 +0100

    e nwm te miesice link

--- src/pages/Dasboard/Options/Calendar/CalendarMain.tsx
+++ src/pages/Dasboard/Options/Calendar/CalendarMain.tsx
@@ -1,18 +1,19 @@
 import { Tab, Tabs } from "@nextui-org/react";
 import Calendar from "./Calendar";
 import CalendarFull from "./CalendarFull";
+import CalendarMonths from "./CalendarMonths";
 
 export default function CalendarMain() {
     return (
-        <Tabs className="flex w-full flex-col px-5 pt-2" color="primary">
+        <Tabs className="flex w-full h-fit flex-col pb-0 px-5 pt-2" color="primary">
             <Tab title="wydarzenia">
                 <Calendar/>
             </Tab>
-            <Tab title="kalendarz">
+            <Tab title="kalendarz" className="pb-0">
                 <CalendarFull/>
             </Tab>
             <Tab title="miesiące">
-
+                <CalendarMonths/>
             </Tab>
         </Tabs>
     )
