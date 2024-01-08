95ba97ea0db886191b54754d7660991d48fcc5fe
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-01-09 00:25:17 +0100

    tez cos css

--- src/pages/Dasboard/Options/Calendar/EventsDetail.tsx
+++ src/pages/Dasboard/Options/Calendar/EventsDetail.tsx
@@ -137,7 +137,7 @@ export default function EventDetails(props: Props) {
         );
     }
     return (
-        <div className="my-2 mx-5 lg:w-[70%] h-full">
+        <div className="my-2 mx-5 lg:w-[70%] h-[calc(100vh/1.2)]">
             <Card className="w-full h-full">
                 <CardHeader className="flex flex-row justify-between">
                     <div className="flex flex-col items-start">
