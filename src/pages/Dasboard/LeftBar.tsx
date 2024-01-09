5c6d4aed8dbecd216bc46f4db646428178397097
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-01-09 21:29:23 +0100

    to tez usuniecie mythings

--- src/pages/Dasboard/LeftBar.tsx
+++ src/pages/Dasboard/LeftBar.tsx
@@ -4,7 +4,6 @@ export default function LeftBar() {
     return (
         <div className="w-[15%] bg-background lg:block sm:hidden xs:hidden">
                 <div className="flex flex-col my-2 ml-5">
-                    <Link href="/Dashboard/MyThings">Moje Rzeczy</Link>
                     <Link href="/Dashboard/Calendar">Kalendarz</Link>
                 </div>
                 <div className="flex flex-col my-2 ml-5">
