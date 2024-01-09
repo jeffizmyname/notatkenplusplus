c03fafa5ecaf1c25db56f1f068709e7569e19aef
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-01-09 21:28:50 +0100

    przerobione wyswietlanie plikow na tabelki

--- src/pages/Dasboard/FilesSites/BlankFiles/BlankFiles.tsx
+++ src/pages/Dasboard/FilesSites/BlankFiles/BlankFiles.tsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react";
 import { getElements } from "../../../../utils/saveLoad";
 import { getId } from "../../../../utils/userData";
 import CardElement, { ModalDefaultContent } from "../../../../components/CardElement";
-import { Modal, ModalContent, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, getKeyValue } from "@nextui-org/react";
+import { Modal, ModalContent, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, useDisclosure } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import OpenBlankTT from "./OpenBlankTT";
+import DeleteBlankTT from "./DeleteBlankTT";
 
 interface NotesItem {
   id: number;
@@ -13,17 +17,6 @@ interface NotesItem {
   CreationDate: string;
   Data: string;
 }
-
-//to jest do zrobienia tylko teraz tego modala treba ogarnąć i bedzie git poradzisz sobie
-
-const rows = [
-  {
-    key: "1",
-    nazwa: "OOF",
-    autor: "JA",
-    datautworzenia: "123 123"
-  }
-]
 
 const columns = [
   {
@@ -38,32 +31,101 @@ const columns = [
     key: "CreationDate",
     label: "DATA UTWORZENIA",
   },
+  {
+    key: "actions",
+    label: "AKCJE"
+  }
 ];
 
+let note: NotesItem | undefined;
+
 export default function BlankFiles() {
+  const navigate = useNavigate();
   const [userNotes, setUserNotes] = useState<NotesItem[]>([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+
 
   useEffect(() => {
+    const updateUserNotes = (notes: NotesItem[]) => {
+      return notes.map((note) => ({
+      ...note,
+      CreationDate: formatCreationDate(note.CreationDate)
+    }));}
+
     async function fetchData() {
       try {
         const userData = await getElements("blank", getId()!)
-        setUserNotes(userData);
+        setUserNotes(updateUserNotes(userData));
       } catch (error) {
         console.error('Error:', error);
       }
     }
 
     fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 1000); 
+
+    return () => clearInterval(intervalId);
   }, []);
 
+  type Note = typeof userNotes[0];
+  const renderCell = React.useCallback((note: NotesItem, columnKey: React.Key) => {
+    const cellValue = note![columnKey as keyof Note];
+    switch (columnKey) {
+      default:
+        return (
+          <div className="m-2">
+            {cellValue}
+          </div>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <OpenBlankTT id={note.id}/>
+            <DeleteBlankTT id={note.id}/>
+          </div>
+        );
+    }
+
+  }, []);
+
+  function formatCreationDate(dateString: string) {
+    const formattedDate = new Date(dateString).toLocaleString('en-US', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    });
+    return formattedDate;
+  }
+
+  const findNoteById = (id: number) => {
+    return userNotes.find(note => note.id === id);
+  };
+
+  const handleClick = (key: number | bigint | string) => {
+    note = findNoteById(Number(key))
+    onOpen()
+  }
+
+  const handleOpen = () => {
+    navigate(`/dashboard/blank/${note!.id}`)
+  }
+
   return (
-    <div className="m-4 mr-0">
+    <div className="m-4 mx-6">
       <div>
-        <p
-          className="m-2 text-xl">Twoje notatki</p>
-        <div className="flex flex-col gap-5 overflow-y-scroll mr-10">
+        <div className="flex flex-row justify-between mb-4">
+        <p className="m-2 text-3xl">Twoje notatki</p>
           <CardElement type="new" category="blank" fileId={0} ListName="" Author="" Desc="" CreationDate="" />
-          {JSON.stringify(userNotes)}
+        </div>
+        <div className="flex overflow-y-scroll">
+          {/* {JSON.stringify(userNotes)} */}
           {/* {userNotes.map((note, index) => (
 
               <CardElement
@@ -78,23 +140,26 @@ export default function BlankFiles() {
               />
             ))} */}
           <Table
-          onRowAction={(key) => alert(`Opening item ${key}...`)}>
+            isStriped 
+            aria-label="e"
+            onRowAction={(key) => handleClick(key)}>
             <TableHeader columns={columns}>
               {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
             </TableHeader>
             <TableBody items={userNotes}>
               {(item) => (
                 <TableRow key={item.id}>
-                  {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+                  {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
                 </TableRow>
               )}
             </TableBody>
           </Table>
-          <Modal>
+          <Modal isOpen={isOpen} onClose={onClose}>
             <ModalContent>
-              {/* <ModalDefaultContent/> */}
-              e
-            </ModalContent>
+              {note !== undefined ?
+              <ModalDefaultContent handleOpen={handleOpen} onClose={onClose} name={note.Name} author={note.Author} desc={note.Description} />
+              : "dudu"}
+              </ModalContent>
           </Modal>
         </div>
       </div>
--- src/pages/Dasboard/FilesSites/ToDoFiles/ToDoFiles.tsx
+++ src/pages/Dasboard/FilesSites/ToDoFiles/ToDoFiles.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
-import CardElement from "../../../../components/CardElement";
+import CardElement, { ModalDefaultContent } from "../../../../components/CardElement";
 import { getId } from "../../../../utils/userData";
 import { getElements } from "../../../../utils/saveLoad";
+import { Modal, ModalContent, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, useDisclosure } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import OpenToDoTT from "./OpenToDoTT";
+import DeleteToDoTT from "./DeleteToDoTT";
 
 interface TodoItem {
   id: number;
@@ -11,33 +16,121 @@ interface TodoItem {
   CreationDate: string
 }
 
+const columns = [
+  {
+    key: "Name",
+    label: "NAZWA",
+  },
+  {
+    key: "Author",
+    label: "AUTOR",
+  },
+  {
+    key: "CreationDate",
+    label: "DATA UTWORZENIA",
+  },
+  {
+    key: "actions",
+    label: "AKCJE"
+  }
+];
+
+let todo: TodoItem | undefined;
+
+
 export default function ToDoFiles() {
+  const navigate = useNavigate();
   const [userTODOs, setUserTODOs] = useState<TodoItem[]>([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
 
   useEffect(() => {
+
+    const updateUserToDos = (notes: TodoItem[]) => {
+      return notes.map((note) => ({
+        ...note,
+        CreationDate: formatCreationDate(note.CreationDate)
+      }));
+    }
+
+
     async function fetchData() {
       try {
         const userData = await getElements("todo", getId()!)
-        setUserTODOs(userData);
+        setUserTODOs(updateUserToDos(userData));
+
       } catch (error) {
         console.error('Error:', error);
       }
     }
 
     fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 1000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
-  console.log(userTODOs);
+  type Todo = typeof userTODOs[0];
+  const renderCell = React.useCallback((todo: TodoItem, columnKey: React.Key) => {
+    const cellValue = todo![columnKey as keyof Todo];
+    switch (columnKey) {
+      default:
+        return (
+          <div className="m-2">
+            {cellValue}
+          </div>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <OpenToDoTT id={todo.id}/>
+            <DeleteToDoTT id={todo.id}/>
+          </div>
+        );
+    }
+
+  }, []);
+
+  function formatCreationDate(dateString: string) {
+    const formattedDate = new Date(dateString).toLocaleString('en-US', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    });
+    return formattedDate;
+  }
+
+  const findToDoById = (id: number) => {
+    return userTODOs.find(todo => todo.id === id);
+  };
+
+  const handleClick = (key: number | bigint | string) => {
+    todo = findToDoById(Number(key))
+    onOpen()
+  }
+
+  const handleOpen = () => {
+    navigate(`/dashboard/todo/${todo!.id}`)
+  }
+
+  //console.log(userTODOs);
 
 
   return (
-    <div className="m-4 mr-0">
+    <div className="m-4 mx-6">
       <div>
-        <p
-        className="m-2 text-xl">Dzisiaj</p>
-        <div className="flex flex-row gap-5 overflow-y-scroll">
-        <CardElement type="new" category="todo" fileId={0} ListName="" Author="" Desc="" CreationDate=""/>
-          {userTODOs.map((todo, index) => (
+        <div className="flex flex-row justify-between mb-4">
+          <p className="m-2 text-3xl">Twoje TODO</p>
+          <CardElement type="new" category="todo" fileId={0} ListName="" Author="" Desc="" CreationDate="" />
+        </div>
+        <div className="flex overflow-y-scroll">
+          {/* {userTODOs.map((todo, index) => (
             <CardElement
               key={index}
               type="todo"
@@ -48,7 +141,29 @@ export default function ToDoFiles() {
               Desc={todo.Description}
               CreationDate={todo.CreationDate} 
             />
-          ))}
+          ))} */}
+          <Table
+            isStriped
+            aria-label="e"
+            onRowAction={(key) => handleClick(key)}>
+            <TableHeader columns={columns}>
+              {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+            </TableHeader>
+            <TableBody items={userTODOs}>
+              {(item) => (
+                <TableRow key={item.id}>
+                  {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalContent>
+              {todo !== undefined ?
+                <ModalDefaultContent handleOpen={handleOpen} onClose={onClose} name={todo.Name} author={todo.Author} desc={todo.Description} />
+                : "dudu"}
+            </ModalContent>
+          </Modal>
         </div>
       </div>
     </div>
